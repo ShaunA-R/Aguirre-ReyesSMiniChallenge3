2d3aeaee3e85baf9297d01beb00532bacfae81cc
ShaunA-R <shaunreyes209@gmail.com>
2022-10-17 15:03:31 -0700

    FirstPush

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Shaun Aguirre-Reyes
+//10/17/22
+//3:02pm
+//This is just a simple conversation where the computer asks a question then reads the users input
+
+
+Console.Clear();
+
+string playAgain = "yes";
+while(playAgain == "yes"){
+
+Console.WriteLine("What is Your Name?");
+
+string myName = Console.ReadLine();
+
+Console.WriteLine("Good Afternoon " + myName);
+
+Console.WriteLine("What time did you wake up at?");
+
+string Time = Console.ReadLine();
+
+Console.WriteLine("Wow you woke up at " + Time + ", thats amazing to hear " + myName + " I hope you have an amazing day :)");
+
+Console.WriteLine("Would you like to talk again?");
+playAgain = Console.ReadLine();
+if(playAgain == "yes"){
+    Console.WriteLine("Lets play again");
+}else{
+    Console.WriteLine("Have a great day :)");
+}
+
+}
